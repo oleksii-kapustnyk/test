d40e6d1f1e1885542036d4a250ddb8059deac601
Oleksii Kapustnyk <oleksii.kapustnyk@gmail.com>
2022-03-21 20:30:34 +0200

    help

--- src/com/ua/Main.java
+++ src/com/ua/Main.java
@@ -3,6 +3,6 @@ package com.ua;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Help me please!!!");
     }
 }
